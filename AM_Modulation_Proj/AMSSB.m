5202c63581bee76e69831a080470f217fd1f97ab
Nick Molo <nick.molo@gmail.com>
2014-12-05 15:51:56 -0500

    got it working

--- AM_Modulation_Proj/AMSSB.m
+++ AM_Modulation_Proj/AMSSB.m
@@ -3,123 +3,49 @@ function [y] = AMSSB(ch)
 % ch = 1, 2, 3, 4, 5, 6
 % ch is the channel you want to tune in to
 
-% MODULATION was done in function AMSSB_mod.m
-% Givens from modulation
-Wm = 142;
-w_a = 2 * Wm; % Wanted 0-1khz
-w_b = 2.25 * w_a; % Wanted 1.25-2.25kHz
-w_c = 3.5 * w_a; % Wanted 2.5-3.5kHz
-w_d = 4.75 * w_a; % Wanted 3.75-4.75kHz
-w_e = 6 * w_a; % Wanted 5-6kHz
-w_f = 7.25 * w_a; % Wanted 6.25-7.25kHz
-
-% Other givens
+%% Givens
 duration = 8;
 f_sample = 44100;
 t = (((0-4)*f_sample+0.5):((duration-4)*f_sample-0.5))/f_sample;
-[radio2,~]=audioread('radio2.wav');
-x = radio2';
-f = f_sample * (1:20000)/100000;
 
+W = 2*pi*1000;
+h = sin(W * t) ./ (pi * t);
 
-% DEMODULATION
-if ch == 1
-    w_cutoff = w_a;
-    w_shift = 0;
-    a = 4;
-    attenuation = 100;
+%% Chosen carrier frequencies
+fc1 = 2*pi*1000;
+fc2 = 2*pi*2100;
+fc3 = 2*pi*3200;
+fc4 = 2*pi*4300;
+fc5 = 2*pi*5400;
+fc6 = 2*pi*6500;
+
+%% Demodulation
+
+[radio2,~] = audioread('radio2.wav');
+radio2=radio2';
+
+%% choosing which channel to restore
+if ch == 1 
+    k = fc1;
 elseif ch == 2
-    w_cutoff = w_b;
-    w_shift = 3.5;
-    a = 1.5;
-    attenuation = 1;
+    k = fc2;
 elseif ch == 3
-    w_cutoff = w_c;
-    w_shift = 4.5;
-    attenuation = 100;
-    a = 1;
+    k = fc3;
 elseif ch == 4
-    w_cutoff = w_d;
-    w_shift = 4.9;
-    attenuation = 100;
-    a = .7;
+    k = fc4;
 elseif ch == 5
-    w_cutoff = w_e;
-    w_shift = 5.2;
-    attenuation = 100;
-    a = .55;
+    k = fc5;
 elseif ch == 6
-    w_cutoff = w_f;
-    w_shift = 5.4;
-    attenuation = 100;
-    a = .5;
-else
-    disp('Inappropriate channel value given');
+    k = fc6;
 end
 
-% BP filter that slides to pick up desired channel
-h_bpf =  2*w_cutoff*sinc(2*w_cutoff*t) .* sin(a*w_cutoff * t) .* exp(1i * -1000 *t);
-output = ece301conv(h_bpf,x);
+bpf = sin(k*t)./(pi*t) - sin((k-2*pi*1000)*t)./(pi*t);
+w_bpf = ece301conv(radio2, bpf);
+w = w_bpf .* 4.*cos(k * t);
+
+w = ece301conv(w, h);
+soundsc(w, f_sample);
 
 
-
-subplot(4,1,1);
-my2fft = abs(fft(x));
-my2fft = my2fft(1:20000);
-plot(f, my2fft);
-axis([0, 8000, 0, 200]);
-
-subplot(4,1,2);
-myfft = abs(fft(output));
-myfft = myfft(1:20000);
-plot(f, myfft);
-axis([0, 8000, 0, 10]);
-
-% soundsc(output, f_sample);
-
-% Shift channel to center
-
-out_shift = output .* exp(-1i*(w_shift)*w_cutoff*t);
-
-%.* cos((w_cutoff+1500*ch)*t) 
-
-subplot(4,1,3);
-myfft = abs(fft(out_shift));
-myfft = myfft(1:20000);
-plot(f, myfft);
-axis([0, 8000, 0, 200]);
-
-
-% Single out left frequency and remove right frequency
-%h_lpf_2 = 2*Wm*sinc((Wm)*t) .* cos(pi*(Wm)*t) .* exp(-1i*2*pi*Wm*t);
-h_bpf2 =  2*w_cutoff*sinc(2*w_cutoff*t) .* sin(a*w_cutoff * t);
-
-subplot(4,1,3);
-myfft = abs(fft(h_bpf2.* exp(-1i*(-20)*w_cutoff*t)));
-myfft = myfft(1:20000);
-plot(f, myfft);
-
-
-out_opposite = ece301conv(h_hpf,out_shift);
-
-out_right = out_shift - out_opposite;
-
-
-subplot(4,1,4);
-myfft = abs(fft(out_opposite.* exp(-1i*(-20)*w_cutoff*t)));
-myfft = myfft(1:20000);
-plot(f, myfft);
-axis([0, 8000, 0, 200]);
-
-% Flip to get the right half
-%out_right = conj(out_left);
-
-% Add halves to get final demodulated output
-% Multiply by 4 to correct magnitude loss via filtering
-%out_final = 4 *(out_right + out_left);
-
-% Save the final demodulated output
-%soundsc(out_final* attenuation, f_sample);
-%wavwrite(out_final * attenuation,f_sample,'output.wav');
 
 end
--- AM_Modulation_Proj/GenerateRadio2.m
+++ AM_Modulation_Proj/GenerateRadio2.m
@@ -5,8 +5,6 @@
 duration = 8;
 f_sample = 44100;
 t = (((0-4)*f_sample+0.5):((duration-4)*f_sample-0.5))/f_sample;
-f = f_sample * (1:20000)/100000;
-f2 = f_sample * (-9999:10000)/100000;
 
 %% Load the samples
 [x1, ~]=audioread('x1.wav');
@@ -29,8 +27,8 @@ x6=x6';
 
 %% Initinal low pass filter
 
-W=1000/pi;
-h = 2 * W * sinc(2 * W * t);
+W = 2*pi*1000;
+h = sin(W * t) ./ (pi * t);
 
 %% Putting signals through low pass filter
 
@@ -45,39 +43,30 @@ x6_lpf = ece301conv(x6, h);
 %% Construct AM DSB signals
 
 % Chosen carrier frequencies
-fc1 = 320*pi;
-fc2 = 1000*pi;
-fc3 = 1700*pi;
-fc4 = 2400*pi;
-fc5 = 3100*pi;
-fc6 = 3850*pi;
-
-% Modulating filtered signal over carrier frequency
-y1 = x1_lpf .* cos(fc1* t);
-y2 = x2_lpf .* cos(fc2* t);
-y3 = x3_lpf .* cos(fc3* t);
-y4 = x4_lpf .* cos(fc4* t);
-y5 = x5_lpf .* cos(fc5* t);
-y6 = x6_lpf .* cos(fc6* t);
+fc1 = 2*pi*1000;
+fc2 = 2*pi*2100;
+fc3 = 2*pi*3200;
+fc4 = 2*pi*4300;
+fc5 = 2*pi*5400;
+fc6 = 2*pi*6500;
 
 %% Single Side band lpf filter
 
-% Removing lower side frequencies 
-W1 = fc1/pi;
-W2 = fc2/pi;
-W3 = fc3/pi;
-W4 = fc4/pi;
-W5 = fc5/pi;
-W6 = fc6/pi;
+% Removing lower side 
+x1_ssb_lpf = sin(fc1 * t) ./ (pi * t);
+x2_ssb_lpf = sin(fc2 * t) ./ (pi * t);
+x3_ssb_lpf = sin(fc3 * t) ./ (pi * t);
+x4_ssb_lpf = sin(fc4 * t) ./ (pi * t);
+x5_ssb_lpf = sin(fc5 * t) ./ (pi * t);
+x6_ssb_lpf = sin(fc6 * t) ./ (pi * t);
 
-
-% Removing lower side filters
-x1_ssb_lpf = 2 * W1 * sinc(2 * W1 * t) .* sin(fc1 * t);
-x2_ssb_lpf = 2 * W2 * sinc(2 * W2 * t) .* sin(fc2 * t);
-x3_ssb_lpf = 2 * W3 * sinc(2 * W3 * t) .* sin(fc3 * t);
-x4_ssb_lpf = 2 * W4 * sinc(2 * W4 * t) .* sin(fc4 * t);
-x5_ssb_lpf = 2 * W5 * sinc(2 * W5 * t) .* sin(fc5 * t);
-x6_ssb_lpf = 2 * W6 * sinc(2 * W6 * t) .* sin(fc6 * t);
+% Modulating filtered signal over carrier frequency
+y1 = x1_lpf .* cos(fc1 * t);
+y2 = x2_lpf .* cos(fc2 * t);
+y3 = x3_lpf .* cos(fc3 * t);
+y4 = x4_lpf .* cos(fc4 * t);
+y5 = x5_lpf .* cos(fc5 * t);
+y6 = x6_lpf .* cos(fc6 * t);
 
 % convolution 
 z1 = ece301conv(y1, x1_ssb_lpf); 
@@ -89,87 +78,35 @@ z6 = ece301conv(y6, x6_ssb_lpf);
 
 % adding signals together
 total = z1 +z2 +z3 +z4 + z5 + z6;
-
 %% Demodulation
 
-r1 = ece301conv(total, x1_ssb_lpf);
-r2 = ece301conv(total, x2_ssb_lpf);
-r3 = ece301conv(total, x3_ssb_lpf);
-r4 = ece301conv(total, x4_ssb_lpf);
-r5 = ece301conv(total, x5_ssb_lpf);
-r6 = ece301conv(total, x6_ssb_lpf);
+audiowrite('radio2.wav', total, f_sample );
 
-s1 = r1 .* exp(-1i * fc1 * t);
-s2 = r2 .* exp(-1i * fc2 * t);
-s3 = r3 .* exp(-1i * fc3 * t);
-s4 = r4 .* exp(-1i * fc4 * t);
-s5 = r5 .* exp(-1i * fc5 * t);
-s6 = r6 .* exp(-1i * fc6 * t);
-
-t1 = ece301conv(r1, h);
-t2 = ece301conv(r2, h);
-t3 = ece301conv(r3, h);
-t4 = ece301conv(r4, h);
-t5 = ece301conv(r5, h);
-t6 = ece301conv(r6, h);
-
-u1 = r1 .* 4;
-u2 = r2 .* 8;
-u3 = r3 .* 4;
-u4 = r4 .* 4;
-u5 = r5 .* 4;
-u6 = r6 .* 4;
-
- 
-
-
-%% plot stuff yo
 % 
-figure(1);
-myfft1 = abs(fft(x2_ssb_lpf));
-myfft1 = myfft1(1:20000);
-myfft = abs(fft(total));
-myfft = myfft(1:20000);
-plot(f, myfft,f, myfft1);
-axis([0, 10000, 0, 150]); 
-
-
-figure(2);
-subplot(5,1,1);
-y1fft = abs(fft(x2_lpf));
-y1fft = y1fft(1:20000);
-plot(f, y1fft);
-title('original lpf signal');
-
-subplot(5,1,2);
-y1fft = abs(fft(r2));
-y1fft = y1fft(1:20000);
-plot(f, y1fft);
-title('after pass through of carrier lpf');
-
-
-subplot(5,1,3);
-y2fft = abs(fft(x2_lpf));
-y2fft = y2fft(1:20000);
-y1fft = abs(fft(s2));
-y1fft = y1fft(1:20000);
-plot(f, y1fft, f, y2fft);
-title('second lpf and shift');
-axis([0, 1000, 0, 1000]);
-
-
-subplot(5,1,4);
-plot(t, x2_lpf, t, u2);
-title('original signal overlayed demodulated signal');
- 
-subplot(5,1,5);
-plot(t, x2_lpf, t, u2);
-axis([-3, -2.95, -0.08 0.08]);
-title('zoomed above');
-
- 
-%% Write back sound to file 
-
-%soundsc(x2_lpf, f_sample);
-%soundsc(real(u2), f_sample);
+% [radio2,f_sample,N] = wavread('radio2');
+% radio2=radio2';
+% 
+% 
+% ch = 5;
+% % choosing which channel to restore
+% if ch == 1 
+%     k = fc1;
+% elseif ch == 2
+%     k = fc2;
+% elseif ch == 3
+%     k = fc3;
+% elseif ch == 4
+%     k = fc4;
+% elseif ch == 5
+%     k = fc5;
+% elseif ch == 6
+%     k = fc6;
+% end
+% 
+% bpf = sin(k*t)./(pi*t) - sin((k-2*pi*1000)*t)./(pi*t);
+% w_bpf = ece301conv(radio2, bpf);
+% w = w_bpf .* 4.*cos(k * t);
+% 
+% w = ece301conv(w, h);
+% soundsc(w, f_sample);
 
